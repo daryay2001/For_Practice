9c291012095d190ce87ea379ee5af811c27a6ce1
Daria_Yakymova <yakymovadaria@gmail.com>
2024-03-26 22:07:29 +0200

    Added word Hello

---
+++ My_files/math.py
@@ -0,0 +1,26 @@
+def add(a, b):
+    print(f"result = {a + b}")
+
+
+def sub(a, b):
+    print(f"result = {a - b}")
+
+
+def mult(a, b):
+    print(f"result = {a * b}")
+
+
+def div(a, b):
+    print(f"result = {a / b}")
+
+
+num1 = int(input("Enter your first digit: "))
+num2 = int(input("Enter your second digit: "))
+
+try:
+    div(num1, num2)
+except Exception as error:
+    print(error)
+
+print("Hello")
+
